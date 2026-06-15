6514c5db456b477f220ffa51a8f586822f4236f1
Allan Gabriel <allans23211467-2@alunos.unicesumar.edu.br>
2026-06-14 21:58:39 -0300

    Adicionado as telas do aluno Arthur

--- src/app/iniciar-venda.tsx
+++ src/app/iniciar-venda.tsx
@@ -1,31 +1,132 @@
-// ⚠️ AVISO DE ÍCONES: Os ícones devem ser importados exclusivamente do ficheiro `src/theme/icons.js`.
-// ⚠️ AVISO FIGMA: Não adivinhem tamanhos de letra ou margens. Olhem sempre a aba 'Tipografia' ou 'Inspect' no Figma.
-// Este é o ecrã Iniciar Venda.
-// View funciona como uma caixa/estrutura para organizar elementos.
-// Text mostra texto na tela.
-// StyleSheet serve para criar estilos parecidos com CSS.
-import { StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
-// IniciarVendaScreen é um componente funcional simples.
-export default function IniciarVendaScreen() {
-  // A View centraliza o conteúdo do ecrã.
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Iniciar Venda</Text>
+    <View style={styles.tudo}>
+      <View style={styles.botaocima}>
+        <TouchableOpacity style={styles.click}>
+          <Text style={styles.voltar}>X</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cliente}>
+        <Text style={styles.nome}>Nome Cliente</Text>
+        <Text style={styles.cpf}>Cpf (opcional)</Text>
+      </View>
+
+      <View style={styles.pesquisa}>
+        <Text style={styles.produtos}>Produtos</Text>
+        <TextInput style={styles.barra} placeholder="Pesquisar" />
+        <FlatList />
+      </View>
+
+      <View style={styles.lista}>
+        <Text style={styles.itens}>Itens</Text>
+
+        <View style={styles.card}>
+          <Text style={styles.produto}>Coxinha de Carne</Text>
+          <Text style={styles.deleta}>lixo</Text>
+          <Text style={styles.qntd}>3</Text>
+          <Text style={styles.adiciona}>+</Text>
+        </View>
+        <Text style={styles.preco}>R$8,50</Text>
+      </View>
+
+      <TouchableOpacity style={styles.botao}>
+        <Text style={styles.continua}>Continuar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-// styles guarda os estilos deste ecrã.
 const styles = StyleSheet.create({
-  // container centraliza tudo na horizontal e vertical.
-  container: {
+  tudo: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#F4F4F6",
   },
-  // title define o aspeto do texto do ecrã.
-  title: {
-    fontSize: 24,
+  botaocima: {
+    flex: 1,
+    marginTop: 12,
+    marginLeft: 10,
+  },
+  voltar: {
+    fontSize: 30,
+    color: "#053225",
+  },
+  cliente: {
+    flex: 1,
+    marginLeft: 10,
+  },
+  nome: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#0532259C",
+  },
+  cpf: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#0532259C",
+  },
+  pesquisa: {
+    flex: 1,
+    marginLeft: 10,
+  },
+  produtos: {
+    marginBottom: 12,
+    marginLeft: 2,
+  },
+  barra: {
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: "white",
+  },
+  lista: {
+    flex: 1,
+    marginLeft: 10,
+  },
+  itens: {
+    fontSize: 18,
+    color: "#053225",
+  },
+  card: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  produto: {
+    fontWeight: "bold",
+    marginRight: 100,
+  },
+  deleta: {
+    marginRight: 20,
+  },
+  qntd: {
+    marginRight: 20,
+  },
+  adiciona: {
+    marginRight: 20,
+  },
+  preco: {
+    fontSize: 16,
+  },
+  botao: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  continua: {
+    borderRadius: 10,
+    paddingHorizontal: 100,
+    paddingVertical: 10,
+    backgroundColor: "#3EC300",
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontSize: 17,
   },
 });
